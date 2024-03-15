647184c1bd20972717709f0adbccea0b2ba7eb16
Gabrielly Sendeski <gabrielly.sendeski@ciahering.com.br>
2024-03-15 03:29:53 -0300

    estrutura página principal, titulo Tags e botão Create

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,19 @@
+import { Plus } from "lucide-react";
+
 export function App() {
   return (
-    <h1>Hello Word</h1>
+    <div className="py-10 space-y-8">
+      <div>
+        header
+        tabs
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <h1 className="text-xl font-bold">Tags</h1>
+        <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
+          <Plus className="size-3" />
+          Create new
+          </button>
+      </main>
+    </div>
   )
 }
